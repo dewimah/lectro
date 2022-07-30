f92e17801186a9df34421d58df5a9eccca5025cd
dewi <dewimaharani092001@gmail.com>
2022-07-31 01:43:36 +0700

    ngngbnb

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -47,7 +47,7 @@ class MonitoringController extends Controller
          //$data = DB::table('monitorings')->select('*')->where('battery_id',$id)->get();
          return Monitoring::with('battery')>where('battery_id',$id)->get();
         //return Monitoring::with('battery')->where('id', $id)->get();
-        return $data;
+        //return $data;
         // return ResponseFormatter::success($data,'Berhasil Melakukan Registrasi');
        // ->join('batteries','monitorings.battery_id','=','batteries.id')->get();
     }
